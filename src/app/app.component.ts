e2ecb315a7cf02bf0e9bbc64cf3698ad118c07d1
zhengqijun <zhengqijun@qq.com>
2018-01-25 16:44:42 +0800

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  constructor() { }
+
+  plusReady (){
+    this.title = "plusReady";
+    window["plus"].camera.getCamera().captureImage(function(p){
+      console.dir(p);
+    });	
+  }
+
+  ngOnInit() {
+    if(window["plus"]){
+      this.plusReady();
+    } else {
+      document.addEventListener('plusready',this.plusReady,false);
+    }
+  }
+
+
+
+
+
 }
